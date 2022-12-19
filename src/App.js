6b4182dc5f4f77a902ec0973b1ccf0dc0969a270
John Robin Llanzana <85091831+Kelxety@users.noreply.github.com>
2022-12-19 15:36:45 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+    <div className="bg-gray-900 min-h-screen max-w-7xl items-center flex flex-col justify-center">
+      <header className="flex items-start justify-center p-2">
+        <img src="Octocat.png" className="h-10 w-10" alt="logo" />
+        <p className="text-white">
+          GitHub Codespaces <span className="text-red-500">♥️</span> React
         </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
+      </header>
+      <p className="w-full mx-auto flex bg-white items-center justify-center">
+          Edit {" "} {" - "} {" "} <code>src/App.js</code> and save to reload.
         </p>
-        <p>
+        <p className="flex items-center justify-center text-white">
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -21,7 +22,6 @@ function App() {
             Learn React
           </a>
         </p>
-      </header>
     </div>
   );
 }
